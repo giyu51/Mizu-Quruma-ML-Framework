b00cabb410312ece77fdbd627f865b8cb876ef5e
Giyu <131558924+giyu51@users.noreply.github.com>
2024-02-28 03:39:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="MizuQuruma",
     version="1.0",
     packages=find_packages(),
-    install_requires=["numpy", "colorama", "icecream"],
+    install_requires=["numpy", "colorama"],
 )
