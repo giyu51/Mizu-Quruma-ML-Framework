872280bb76ec409ea21fa9cb6276e39a42aa5cc1
giyu51 <riadhajiyev61@gmail.com>
2024-03-07 10:39:22 +0200

    testing

--- test_file.py
+++ test_file.py
@@ -1,5 +1,26 @@
-from MizuQuruma.internal_methods import display_info, display_warning
+from MizuQuruma.KNearestNeighbours import KNN
+
+model = KNN()
+
+X_train, y_train, X_test, y_test = model.generate_dataset(n_classes=2)
+
+model.fit(X_train, y_train)
+
+prediction = model.predict([[10, 10], [70, 50]])
+print(prediction)
+
+evaluation = model.evaluate(X_test, y_test)
+print(evaluation)
+
+model.save_model()
+
+model = model.load_model()
+evaluation = model.evaluate(X_test, y_test)
+print(evaluation)
 
 
-display_warning("meow")
-display_info("meow")
+my_plot = model.plot_data()
+my_plot.show()
+
+# ---
+
